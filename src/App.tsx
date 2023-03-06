2ef1398a62021419bc29ef31ea0727b95fb5433f
Juubes <grumpt1@gmail.com>
2023-03-06 20:24:58 +0200

    Simple modal done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import './App.css';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {Sidebar} from './components/Sidebar';
-import {ShowView} from "./components/View/ShowView";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Sidebar } from './components/Sidebar';
+import Modal from './components/Modal';
+import { useState } from 'react';
 
 function App() {
-  const queryClient = new QueryClient();
+    const queryClient = new QueryClient();
+    const [modal, setModal] = useState(true);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Sidebar/>
-        <h1>Proto2</h1>
-        <ShowView/>
-      </div>
-    </QueryClientProvider>
-  )
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="w-screen h-screen">
+                <Sidebar />
+
+                <Modal
+                    title="Test"
+                    visible={modal}
+                    close={() => {
+                        setModal(false);
+                    }}
+                >
+                    <h1>Valitse yritys</h1>
+                </Modal>
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,27 +5,37 @@ type ModalProps = {
     title: string;
     children: React.ReactNode;
     close: () => void;
+    visible: boolean;
     footer?: React.ReactNode;
 };
 
+/**
+ * Store the state of the modal in the parent component.
+ */
 function Modal(props: ModalProps) {
+    // floating on top of the page
+    if (!props.visible) return <></>;
     return (
         <>
-            <div>
-                <h2 className="text-2xl font-bold text-blue-900 mb-4">
-                    {props.title}
-                </h2>
+            <div className="fixed h-screen w-screen">
+                <div className="relative p-5 m-5 rounded shadow-2xl bg-white h-screen">
+                    <div className="flex justify-between">
+                        <h1 className="text-6xl font-bold text-blue-900">
+                            {props.title}
+                        </h1>
 
-                <button
-                    onClick={props.close}
-                    className="bg-red-900 text-white px-4 py-2 rounded"
-                >
-                    Sulje
-                </button>
+                        <button
+                            onClick={props.close}
+                            className="bg-red-900 text-white px-4 py-2 rounded"
+                        >
+                            Sulje
+                        </button>
+                    </div>
+                    <hr className="h-px my-8 border-0 bg-gray-700" />
+                    <div className="text-black ">{props.children}</div>
+                    {props.footer}
+                </div>
             </div>
-
-            {props.children}
-            {props.footer}
         </>
     );
 }
